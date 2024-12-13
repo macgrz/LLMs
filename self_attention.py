66cf28f8fbcda108903f73925b7dc46456b46e6d
macgrz <grzelczyk.maciek@gmail.com>
2024-12-13 13:01:22 +0100

    self attention V2

--- self_attention.py
+++ self_attention.py
@@ -19,3 +19,25 @@ class SelfAttention_V1(nn.Module):
         context_vector = attn_weights @ values
 
         return context_vector
+    
+class SelfAttention_V2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)    # uses more sophisticated weight initialization scheme than random nn.Parameter(torch.rand()) method
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+            )
+        context_vector = attn_weights @ values
+
+        return context_vector
