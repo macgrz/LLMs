5bce3569b423035435baa51594a0e27acf96e251
macgrz <grzelczyk.maciek@gmail.com>
2024-12-16 16:00:51 +0100

    casual attention

--- self_attention.py
+++ self_attention.py
@@ -63,3 +63,35 @@ class SelfAttention_V1_with_V2_weights(nn.Module):
         context_vector = attn_weights @ values
 
         return context_vector
+    
+
+class CasualAttention(nn.Module):
+    def __init__(self, d_in, d_out, contex_lenght, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.d_in  = d_in
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(contex_lenght, contex_lenght), diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1,2)
+        attn_scores.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+        attn_weights = self.dropout(attn_weights)
+
+        context_vector = attn_weights @ values
+
+        return context_vector
