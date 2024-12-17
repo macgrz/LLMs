6cf176c376561fbd9392fa8fbb53aa1995a0da40
macgrz <grzelczyk.maciek@gmail.com>
2024-12-17 20:05:13 +0100

    shortcut connections / skip or residual connections

--- models.py
+++ models.py
@@ -12,6 +12,27 @@ GPT_CONFIG_124M = {
     "qkv_bias": False
 }
 
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1])),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2])),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3])),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4])),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5])),
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
 
 
 class GELU(nn.Module):
